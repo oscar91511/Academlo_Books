fb058885714cd9388eaff31247fe4fe7c96f9487
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-07-06 13:47:02 -0500

    add list of fruits whit funtion on ul

--- src/home/page/HomePage.vue
+++ src/home/page/HomePage.vue
@@ -10,13 +10,28 @@ const incrementar = () => {
 const decrementar = () => {
   contador.value--;
 };
+
+const fruts = [        //* List of fruits
+  { id: 1, name: "Apple" },
+  { id: 2, name: "Banana" },
+  { id: 3, name: "Orange" },
+  { id: 4, name: "Strawberry" },
+  { id: 5, name: "Pear" },
+];
 </script>
+
 <template>
   <div>Home Page</div>
   <div>Contador: {{ contador }}</div>
   <button @click="incrementar">Incrementar</button>
   &nbsp;
   <button @click="decrementar">Decrementar</button>
+
+  <ul>
+    
+    <li v-for="frut in fruts" :key="frut.id">{{ frut.name }}</li>
+    
+  </ul>
 </template>
 
 <style scoped></style>
