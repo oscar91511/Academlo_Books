217b8b43263c04d7b7dd39059f111e874621f3e7
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-07-07 20:55:44 -0500

    add athorsts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,23 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-// import { VueQueryPlugin } from "@tanstack/vue-query"
+import { VueQueryPlugin } from "@tanstack/vue-query"
 
 const pinia = createPinia();
 
 const app = createApp(App);
 
-app.use(pinia)
+app.use(pinia);
 app.use(router);
+
+VueQueryPlugin.install(app, {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        cacheTime: 1000 * 60, //* time storage for cache / tiempo de almacenamiento cache
+      },
+    },
+  },
+});
 
 app.mount("#app");
---
+++ src/store/books.ts
@@ -0,0 +1,3 @@
+const numeros: number[] = [1, 2, 3, 4, 5];
+
+
